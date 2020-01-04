fa325b85bdaebc942f1019acfa20805da0c614b0
Anjelica <anjelicacsilva@gmail.com>
2020-01-04 12:30:47 -0800

    Implement showFortune function

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,8 +4,9 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
-
-    // TODO: get the fortune and show it in the #fortune-text div
+  $.get('/fortune', (res) => {
+    $('#fortune-text').text(res);
+  });
 }
 
 $('#get-fortune-button').on('click', showFortune);
@@ -17,10 +18,10 @@ $('#get-fortune-button').on('click', showFortune);
 // PART 2: SHOW WEATHER
 
 function showWeather(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    let url = "/weather.json";
-    let formData = {"zipcode": $("#zipcode-field").val()};
+  let url = "/weather.json";
+  let formData = {"zipcode": $("#zipcode-field").val()};
 
 
     // TODO: request weather with that URL and show the forecast in #weather-info
@@ -34,7 +35,7 @@ $("#weather-form").on('submit', showWeather);
 // PART 3: ORDER MELONS
 
 function orderMelons(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
